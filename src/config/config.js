0bf72743465fba41a174deaec35ccf8a57798b07
AVR216 <jhon.villalba.rubio@gmail.com>
2024-11-17 12:34:34 -0500

    Updates

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-export const API_KEY = 'cd6eaac41d34081b6b5c901e0560fab9';
-
-export const API_URL = 'https://api.openweathermap.org/data/2.5/forecast?';
-
-export const LOCATION_API_URL = 'https://api.openweathermap.org/geo/1.0/direct?';
--- src/utils/temperature/temperature.js
+++
@@ -1,9 +0,0 @@
-export function kelvinToCelsius({kelvin}) {
-    try {
-        const celsius = kelvin - 273.15;
-        return parseFloat(celsius.toFixed(3));
-    } catch (error) {
-        console.log(error.message);
-        throw new Error('Error parsing kelvin to celsius');
-    }
-}
